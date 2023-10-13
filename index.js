e512b01af74fa3548ae95bbc28b7033910708612
LD211 <93799505+LD211@users.noreply.github.com>
2023-10-13 19:53:30 -0400

    automated update.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const input = initel("input");
 let focusIndex = 0;
-const fileTabInformation = []
+let currentTab = "tab";
+const fileTabInformation = {};
 
 class file {
   constructor(name, buffer) {
@@ -17,23 +18,31 @@ function addInputListeners() {
     }
   });
   initel("notepad").addEventListener("mousedown", (mouse) => {
-    console.log(mouse.x);
-
+    // console.log(mouse.x);
   });
   document.addEventListener("DOMContentLoaded", () => {
     initStyles();
+    createListenerMD("tab");
   });
-  initel("file").addEventListener("mousedown", () => {
+  document.addEventListener("keydown", (key) => {
     getLine();
   });
+  initel("file").addEventListener("mousedown", () => {
+    // getLine();
+  });
   initel("edit").addEventListener("mousedown", () => {
-
   });
   initel("view").addEventListener("mousedown", () => {
-
   });
   initel("settings").addEventListener("mousedown", () => {
-
+  });
+  initel("xtab").addEventListener("mousedown", () => {
+    if (currentTab === "tab") return;
+    initel(currentTab).remove();
+    currentTab = "tab" + focusIndex--;
+    if (focusIndex < 0) {
+      currentTab = "tab";
+    }
   });
 }
 
@@ -45,26 +54,56 @@ function saveBuffer() {
 }
 
 function openTab() {
-  const newTab = initel("tab").cloneNode(true);
+  const newTab = initel(currentTab).cloneNode(true);
   const computedStyles = window.getComputedStyle(initel("tab"));
   for (let i = 0; i < computedStyles.length; i++) {
-    newTab.style.setProperty(computedStyles[i], computedStyles.getPropertyValue(computedStyles[i]));
+    newTab.style.setProperty(
+      computedStyles[i],
+      computedStyles.getPropertyValue(computedStyles[i]),
+    );
   }
+  newTab.id = "tab" + focusIndex;
+  currentTab = newTab.id;
 
-  initel("tabsection").insertBefore(newTab, initel("tabsection").children[initel("tabsection").children.length - 1]);
+  initel("tabsection").insertBefore(
+    newTab,
+    initel("tabsection").children[initel("tabsection").children.length - 1],
+  );
+  let oldFile = "tab";
+  if (focusIndex !== 0) {
+    oldFile += focusIndex - 1;
+  }
   focusIndex++;
-  const fle = new file(focusIndex, input.value);
-  fileTabInformation.push(fle);
+  const fle = new file(oldFile, input.value);
+  console.log(oldFile + "line");
+  fileTabInformation[oldFile] = fle;
   input.value = "";
+  createListenerMD(newTab.id);
+}
+
+function createListenerMD(id) {
+  initel(id).addEventListener("mousedown", () => {
+    console.log(id);
+    fileTabInformation[currentTab] = new file(currentTab, input.value);
+    currentTab = id;
+    if (fileTabInformation[id] !== undefined) {
+      input.value = fileTabInformation[id].buffer;
+    } else {
+      fileTabInformation[id] = new file(id, "");
+      input.value = "";
+    }
+  });
 }
 
 function loadFile(index) {
-  input.value = fileTabInformation[index].buffer
+  input.value = fileTabInformation[index].buffer;
 }
 
 function getLine() {
-  console.log(input.ariaColIndex + "\n" + input.ariaColSpan);
-  input.ariaColIndex;
+  const charPosition = input.selectionStart;
+  console.log(charPosition);
+  const row = input.value.split(/\r?\n/).length;
+  console.log(row);
 }
 
 function initel(element) {
@@ -75,7 +114,4 @@ function initStyles() {
   const notepad = initel("notepad");
   const tabsection = initel("tabsection");
   const maintext = initel("maintext");
-
-
-
 }
